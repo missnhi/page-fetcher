6c7c91875253fcedd845681f808dc2cdf1ad8246
missnhi <nhi.phan.ley@gmail.com>
2024-10-09 22:36:46 -0600

    page fetcher basic EDGE CASES

--- fetcher.js
+++ fetcher.js
@@ -19,16 +19,35 @@ DON'T use pipe function or sync functions
 
 const needle = require('needle');
 const fs = require('fs');
+const path = require('path');
 
 // ex: node fetcher.js http://www.example.edu/ ./index.html
 const url = process.argv[2];
 const filePath = process.argv[3];
+
+
+// Check if the file already exists
+if (fs.existsSync(filePath)) {
+  console.error(`Error: The file at ${filePath} already exists.`);
+  process.exit(1);
+}
+// check if the file path is valid
+const dir = path.dirname(filePath);
+if (!fs.existsSync(dir)) {
+  console.error(`Error: The directory ${dir} does not exist.`);
+  process.exit(1);
+}
 
 needle.get(url, (error, response, body) => {
     if (error) {
       console.error(error);
       return error;
     }
+    if (response.statusCode !== 200) {
+      console.error(`Error: Received status code ${response.statusCode}`);
+      return;
+    }
+    
     fs.writeFile(filePath, body, (err) => {
       if (err) {
         console.error(err);
@@ -38,3 +57,14 @@ needle.get(url, (error, response, body) => {
     });
   }
 );
+
+/*
+EDGE CASES
+
+the local file already exists?
+the local file path given is invalid?
+the URL results in an error or non-200 result?
+ */
+
+
+
